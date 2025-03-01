4a1e3f9fc5f710f47ce7afb58079f81a7bf8ec84
Pamal Jayasinghe <157864836+pamaljayasinghe@users.noreply.github.com>
2025-03-02 01:18:28 +0530

    Create 34findandlastpoistion.java

---
+++ 34findandlastpoistion.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = {-1, -1};
+
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+
+        result[0] = findFirstPosition(nums, target);
+
+        if (result[0] == -1) {
+            return result;
+        }
+
+        result[1] = findLastPosition(nums, target);
+
+        return result;
+    }
+
+    private int findFirstPosition(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int result = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                result = mid; 
+                end = mid - 1; 
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    private int findLastPosition(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int result = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                result = mid; 
+                start = mid + 1; 
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
